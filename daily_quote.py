9c5aee0b5cc97171fbbe64acd138cc9b53e9b9ef
Samuel Khoo <115562183+SamuelKhoo2003@users.noreply.github.com>
2024-09-08 18:11:49 +0100

    chore: updated github runner script

--- daily_quote.py
+++ daily_quote.py
@@ -1,35 +1,26 @@
 import requests
 
 def fetch_quote(tags):
-    tags_query = ','.join(tags)
+    tags_query = '|'.join(tags)
     url = f"https://api.quotable.io/quotes/random?tags={tags_query}"
     
     response = requests.get(url)
     response.raise_for_status()
     quote_data = response.json()
-    return quote_data["content"], quote_data["author"]
+    return quote_data[0]["content"], quote_data[0]["author"]
 
 def update_readme(new_quote, new_author):
-    with open("README.md", "r") as file:
+    with open("README.md", "r", encoding='utf-8') as file:
         readme_content = file.readlines()
 
     updated_content = []
-    quote_replaced = False
     for line in readme_content:
-        if line.startswith("## ✨ Quote of the Day ✨"):
-            updated_content.append(f'\n## ✨ Quote of the Day ✨\n\n> "{new_quote}" — **{new_author}**\n')
-            quote_replaced = True
-            continue
-        elif quote_replaced and line.startswith("## "):
-            quote_replaced = False
-            updated_content.append(line)
+        if line.strip().startswith("> "):
+            updated_content.append(f'> "{new_quote}" — **{new_author}**\n')
         else:
             updated_content.append(line)
-    
-    if not quote_replaced:
-        updated_content.append(f'\n## ✨ Quote of the Day ✨\n\n> "{new_quote}" — **{new_author}**\n')
 
-    with open("README.md", "w") as file:
+    with open("README.md", "w", encoding='utf-8') as file:
         file.writelines(updated_content)
 
 if __name__ == "__main__":
